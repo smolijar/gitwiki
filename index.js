5c30045b86d1f2cc39c4ffa08f2eecaa90ae631c
Jaroslav Šmolík <grissius@gmail.com>
2018-02-13 17:44:23 +0100

    Use ssh-add agent instead of app creds loading

--- index.js
+++ index.js
@@ -10,31 +10,18 @@ const promised = (fn, thisCtx) =>
       )
   )
 
-function getGitoliteCredentials() {
-  return promised(fs.readFile)('.gitwiki.config.json', 'utf-8')
-    .then(config => JSON.parse(config))
-    .then(config => NodeGit.Cred.sshKeyNew(
-      config.ssh.user,
-      config.ssh.public,
-      config.ssh.private,
-      config.ssh.passphrase));
-}
-
 const url = "git@localhost:gitolite-admin";
 const dest = "./tmp";
 const cloneOpts = {};
-getGitoliteCredentials()
-  .then(cred => {
-    cloneOpts.fetchOpts = {
-      callbacks: {
-        credentials: function (url, userName) {
-          return cred;
-        }
-      }
-    };
-    NodeGit.Clone(url, dest, cloneOpts).then(function (repo) {
-      console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
-    }).catch(function (err) {
-      console.log(err);
-    });
-  })
+cloneOpts.fetchOpts = {
+  callbacks: {
+    credentials: function (url, userName) {
+      return NodeGit.Cred.sshKeyFromAgent(userName);
+    }
+  }
+};
+NodeGit.Clone(url, dest, cloneOpts).then(function (repo) {
+  console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
+}).catch(function (err) {
+  console.log(err);
+});
