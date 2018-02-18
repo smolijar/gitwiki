39431bb4bb1de81812cf579a739bfd75d1f86314
Jaroslav Šmolík <grissius@gmail.com>
2018-02-18 12:15:21 +0100

    Add repository browsing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Added
+- Local repository cloning
+- Repository browsing
 
 ## [0.0.1] - 2018-02-13
 ### Added
--- src/git.js
+++ src/git.js
@@ -15,3 +15,26 @@ module.exports.getLocalRepository = (repoPath, user) => {
   };
   return NodeGit.Clone(url, dest, cloneOpts)
 }
+
+module.exports.browse = (repo, user, path = null) => {
+  const formatTree = tree => tree.entries().map(e => e.path());
+  const formatBlob = entry => entry.path();
+  return repo.getHeadCommit()
+    .then(commit => commit.getTree())
+    .then(tree => {
+      if (path) {
+        return tree.getEntry(path)
+          .then(entry => {
+            if (entry.isBlob()) {
+              return formatBlob(entry);
+            }
+            else {
+              return entry.getTree()
+                .then(formatTree);
+            }
+          })
+
+      }
+      return formatTree(tree);
+    })
+}
