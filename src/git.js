d0b906dc9610638858bb5efbec73fd553a40c92f
Jaroslav Šmolík <grissius@gmail.com>
2018-02-18 13:07:50 +0100

    Add pull when reusing repo

--- src/git.js
+++ src/git.js
@@ -17,9 +17,15 @@ module.exports.getLocalRepository = (repoPath, user) => {
   };
   return NodeGit.Clone(url, dest, cloneOpts)
     .catch(e => {
+      let repository;
       // exists and is not an empty directory
       if (e.errno === -4) {
         return NodeGit.Repository.open(dest)
+        // fetch does not return repo, must store
+        .then(repo => repository = repo)
+        .then(repo => repo.fetchAll(cloneOpts.fetchOpts))
+        .then(() => repository.mergeBranches('master', 'origin/master'))
+        .then(() => repository)
       }
     })
 }
