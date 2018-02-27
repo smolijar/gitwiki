c1ab9792c7362c3b6558dadbd1d4881d95992260
Jaroslav Šmolík <grissius@gmail.com>
2018-02-27 13:59:01 +0100

    Fix container connection

--- containers/ReferencesContainer.js
+++ containers/ReferencesContainer.js
@@ -1,10 +1,9 @@
 import { connect } from 'react-redux';
-import withRedux from '../redux/withRedux';
 import actions from '../redux/actions/actions';
 import References from '../components/References';
 
 
-export default withRedux()(connect(
+export default connect(
   state => ({ repo: state.repo.meta, refs: state.repo.refs }),
   { fetchRefs: actions.repo.fetchRefs },
-)(References));
+)(References);
