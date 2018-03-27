573492b109822d24de6ae4c4001827743c4eeecb
Jaroslav Šmolík <grissius@gmail.com>
2018-03-27 20:57:33 +0200

    Add gitolite wrapper

---
+++ server/src/gitolite/index.js
@@ -0,0 +1,50 @@
+const { exec } = require('child_process');
+const { not, filter, identity, split, compose, dropLastWhile, trim, all, ifElse, match, join, pipe, map, both, always, concat } = require('ramda');
+
+const bin = '/home/git/bin/gitolite';
+const prefix = 'HOME=/home/git; ' + bin + ' ';
+
+const createCommand = (command, tranform = identity) => () => new Promise((res, rej) => {
+  exec(prefix + command,
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        rej(error, stdout, stderr);
+      }
+      res(stdout, stderr);
+    });
+})
+  .then(tranform);
+
+const procArgs = pipe(
+  dropLastWhile(not),
+  ifElse(
+    all(both(identity, match(/^\s*[a-zA-Z0-9@]+\s*$/))),
+    identity,
+    always([]),
+  ),
+  join(' '),
+  concat(' ')
+);
+
+const commands = {
+  LIST_REPOS: 'list-repos',
+  LIST_USERS: 'list-users',
+  LIST_GROUPS: 'list-groups',
+  ACCESS: 'access',
+}
+
+const outToList = compose(filter(identity), split('\n'));
+
+const gitolite = {
+  listRepos: createCommand(commands.LIST_REPOS, outToList),
+  listUsers: createCommand(commands.LIST_USERS, outToList),
+  listGroups: createCommand(commands.LIST_GROUPS, outToList),
+  access: (repo, user, perm = null, ref = null) => {
+    const command = concat(commands.ACCESS, procArgs([repo, user, perm, ref]));
+    return createCommand(command)()
+      .then(always(true))
+      .catch(err => err.code === 1 ? false : err)
+  }
+}
+
+module.exports = gitolite;
