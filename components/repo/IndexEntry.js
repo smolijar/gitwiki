4f615e0e41336af409251d3847fb93ab04c576e4
Jaroslav Šmolík <grissius@gmail.com>
2018-04-07 16:10:49 +0200

    Add repo links

--- components/repo/IndexEntry.js
+++ components/repo/IndexEntry.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import { Card, Avatar } from 'antd';
+import { unless, equals, always } from 'ramda';
+import { compile } from 'path-to-regexp';
+import { front } from '../../common/endpoints';
 
 const entryType = PropTypes.shape({
   name: PropTypes.string,
+  description: PropTypes.string,
+  provider: PropTypes.string,
 });
 
 export { entryType };
+
+const icon = unless(equals('github'), always('fork'));
 
 export default class IndexEntry extends React.PureComponent {
   static propTypes = {
@@ -15,23 +23,26 @@ export default class IndexEntry extends React.PureComponent {
 
   render() {
     const { entry } = this.props;
+    const params = { ref: 'master', path: '', ...entry };
     return (
       <div>
-        <Card
-          style={{ height: 90 }}
-        >
-          <Card.Meta
-            avatar={<Avatar icon="github" />}
-            title={entry.name}
-            description={entry.description}
-            className="repo-entry"
-            style={{
-              whiteSpace: 'nowrap',
-              textOverflow: 'ellipsis',
-              overflow: 'hidden',
-            }}
-          />
-        </Card>
+        <Link href={{ pathname: '/repo/tree', query: params }} as={compile(front.tree)(params)}>
+          <Card
+            style={{ height: 90 }}
+          >
+            <Card.Meta
+              avatar={<Avatar icon={icon(entry.provider)} />}
+              title={entry.name}
+              description={entry.description}
+              className="repo-entry"
+              style={{
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+              }}
+            />
+          </Card>
+        </Link>
         <style jsx global>{`
         .repo-entry .ant-card-meta-description {
           white-space: nowrap;
