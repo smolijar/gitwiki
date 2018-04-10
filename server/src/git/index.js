e45cd31357d04cbb4a2c21d6c1f89722414ccd74
Jaroslav Šmolík <grissius@gmail.com>
2018-04-10 18:31:21 +0200

    Refactor compact exports

--- server/src/git/index.js
+++ server/src/git/index.js
@@ -3,9 +3,10 @@ const { refs, findRef } = require('./refs');
 const getRepo = require('./getRepo');
 const browse = require('./browse');
 
-
-module.exports.commit = commit;
-module.exports.refs = refs;
-module.exports.findRef = findRef;
-module.exports.getRepo = getRepo;
-module.exports.browse = browse;
+module.exports = {
+  commit,
+  refs,
+  findRef,
+  getRepo,
+  browse,
+}
